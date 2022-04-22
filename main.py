38618c5bb3397927bcba948a6fe92471e1ecbbdc
Дарья <daraafonasenkova74@gmail.com>
2022-04-22 20:01:02 +0300

    Работает prediction, horoscope работает криво

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-# Импортируем необходимые классы.
 import logging
 import random
 
@@ -33,6 +31,20 @@ def start(update, context):
     )
 
 
+def go_text(update, context):
+    if update.message.text == 'Предсказание' or update.message.text == 'предсказание' \
+            or update.message.text == 'предсказание на день' \
+            or update.message.text == 'Предсказание на день':
+        prediction(update, context)
+    elif update.message.text == 'гороскоп' or update.message.text == 'Гороскоп' or update.message.text == 'расскажи про гороскоп'\
+            or update.message.text == 'Расскажи про гороскоп':
+        horoscope(update, context)
+    else:
+        update.message.reply_text(
+            "Не понимаю... Напишите, что вы хотите или попросите помощи.",
+        )
+
+
 def stop(update, context):
     update.message.reply_text("Всего доброго!")
     return ConversationHandler.END
@@ -44,7 +56,6 @@ f.close()
 
 
 def prediction(update, context):
-    if update.message.text == 'Предсказание':
         update.message.reply_text(
             "Хорошо! Дайте-ка подумать...",
         )
@@ -54,10 +65,72 @@ def prediction(update, context):
 
 
 def horoscope(update, context):
-
     update.message.reply_text(
         "Хорошо! Какой у вас знак зодиака?",
     )
+
+    if update.message.text == 'овен' or update.message.text == 'Овен':
+        update.message.reply_text(
+            "Какой замечательный знак! Вам предстоит сделать много полезного. Не исключено, "
+            "что в апреле вы будете принимать участие в каких-то общественных мероприятиях, решать вопросы, "
+            "важные для всех. Старайтесь избегать любых проявлений легкомыслия: оно будет совершенно неуместным."
+            "Поскольку дел будет необычно много, привычный вам жизненный ритм может измениться, от комфортного графика "
+            "придется отказаться. Вы не пожалеете об этом, легко перенесете любые неудобства.",
+        )
+    if update.message.text == 'телец' or update.message.text == 'Телец':
+        update.message.reply_text(
+            "Тельцы просто прекрасны! Эта неделя хорошо подойдет для того, чтобы взяться за что-то новое и необычное."
+            "Творческий потенциал будет"
+            "высоким, у вас появятся хорошие идеи и не возникнет проблем с их осуществлением. "
+            "Станет ясно, как справиться с непростыми задачами, возникшими в последнее время. Будет много неожиданных "
+            "встреч, удивительных совпадений. Старайтесь обращать внимание на все, что выходит за рамки обычного и "
+            "привычного."
+        )
+    if update.message.text == 'Близнецы' or update.message.text == 'близнецы':
+        update.message.reply_text(
+            "Даже привычные дела на этой неделе могут даваться вам сложнее, чем обычно. Это, конечно, не значит,"
+            "что нужно все отложить и ничем не заниматься. Просто будьте готовы к трудностям, не требуйте от себя"
+            "невозможного и не спешите. Неделя не подходит для того, чтобы принимать участие в каких-то авантюрах."
+            "А вот на поддержку близких можно рассчитывать в течение всей недели. Да и в помощи вам не откажут, "
+            "если вы за ней обратитесь. "
+        )
+    if update.message.text == '' or update.message.text == '':
+        update.message.reply_text(
+
+        )
+
+    if update.message.text == '' or update.message.text == '':
+        update.message.reply_text(
+
+        )
+    if update.message.text == '' or update.message.text == '':
+        update.message.reply_text(
+
+        )
+    if update.message.text == '' or update.message.text == '':
+        update.message.reply_text(
+
+        )
+    if update.message.text == '' or update.message.text == '':
+        update.message.reply_text(
+
+        )
+    if update.message.text == '' or update.message.text == '':
+        update.message.reply_text(
+
+        )
+    if update.message.text == '' or update.message.text == '':
+        update.message.reply_text(
+
+        )
+    if update.message.text == '' or update.message.text == '':
+        update.message.reply_text(
+
+        )
+    if update.message.text == '' or update.message.text == '':
+        update.message.reply_text(
+
+        )
 
 
 def advice(update, context):
@@ -85,8 +158,8 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("stop", stop))
 
-    prediction_handler = MessageHandler(Filters.text, prediction)
-    dp.add_handler(prediction_handler)
+    start_handler = MessageHandler(Filters.text, go_text)
+    dp.add_handler(start_handler)
 
     updater.start_polling()
 
